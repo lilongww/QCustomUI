a3173c0e92be8cfa7324a0b7f192d37e756731df
lilong <634916105@qq.com>
2019-08-05 18:11:11 +0800

    IP输入框release

--- src/QCustomUi/QCtmIPEdit.cpp
+++ src/QCustomUi/QCtmIPEdit.cpp
@@ -5,9 +5,46 @@
 #include <QApplication>
 #include <QTextLayout>
 #include <QTimer>
+#include <QAction>
+#include <QClipboard>
+#include <QMenu>
+#include <QDebug>
 
 constexpr int SECTION_COUNT = 4;
 constexpr int SECTION_CURSOR_COUNT = 4;
+
+QTextLayout::FormatRange createFormatRange(int cursor1, int cursor2, const QPalette& palette)
+{
+	QTextLayout::FormatRange range;
+	int local1 = cursor1 % SECTION_CURSOR_COUNT;
+	int local2 = cursor2 % SECTION_CURSOR_COUNT;
+	range.start = qMin(local1, local2);
+	range.length = qMax(local1, local2) - range.start;
+	range.format.setBackground(palette.brush(QPalette::Highlight));
+	range.format.setForeground(palette.brush(QPalette::HighlightedText));
+	return std::move(range);
+}
+
+void moveFormatRange(QTextLayout::FormatRange& range, int beforeCursor, int afterCursor)
+{
+	int before = beforeCursor % SECTION_CURSOR_COUNT;
+	int after = afterCursor % SECTION_CURSOR_COUNT;
+
+	int anchor;
+	auto start = range.start;
+	auto end = range.start + range.length;
+
+	if (end > start && before == start)
+		anchor = end;
+	else if (end > start && before == end)
+		anchor = start;
+	else
+		anchor = before;
+	start = qMin(anchor, after);
+	end = qMax(anchor, after);
+	range.start = start;
+	range.length = end - start;
+}
 
 struct QCtmIPEdit::Impl
 {
@@ -17,49 +54,67 @@ struct QCtmIPEdit::Impl
 	bool readOnly{ false };
 	int cursorPosition{ 0 };
 	int ascent{ 0 };
+	int pressedSection{ 0 };
+	int pressedCursor{ 0 };
+	bool pressed{ false };
 
 	QTextLayout textLayouts[SECTION_COUNT];
 	QTimer timer;
 	bool cursorSwitch{ false };
-	QVector<QTextLayout::FormatRange> fr;
-
+	QVector<QVector<QTextLayout::FormatRange>> selections;
+	QAction* copy, * paste;
 };
 
-QCtmIPEdit::QCtmIPEdit(QWidget *parent)
+QCtmIPEdit::QCtmIPEdit(QWidget* parent)
 	: QWidget(parent)
 	, m_impl(std::make_unique<Impl>())
 {
 	setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
-
+	setContextMenuPolicy(Qt::CustomContextMenu);
+	m_impl->selections.resize(SECTION_COUNT);
 	m_impl->timer.setInterval(qApp->cursorFlashTime() / 2);
-	m_impl->timer.start();
 	setCursor(Qt::IBeamCursor);
 	setFocusPolicy(Qt::StrongFocus);
 	connect(&m_impl->timer, &QTimer::timeout, this, [=]() {
 		m_impl->cursorSwitch = !m_impl->cursorSwitch;
 		update();
 		});
+	connect(this, &QWidget::customContextMenuRequested, this, &QCtmIPEdit::onCustomContextMenuRequested);
 	QMetaObject::invokeMethod(this, "init", Qt::QueuedConnection);
+	initActions();
 }
 
 QCtmIPEdit::~QCtmIPEdit()
 {
 }
 
-void QCtmIPEdit::setIP(const QString& ip)
+void QCtmIPEdit::setIPAddress(const QString& ip)
 {
 	auto tmp = ip.split('.');
 	if (tmp.size() == 4)
 	{
+		for (auto str : tmp)
+		{
+			if (str.size() > 0 && str.size() <= 3)
+			{
+				bool ok = false;
+				auto num = str.toInt(&ok);
+				if (!(num >= 0 && num <= 255 && ok))
+					return;
+			}
+			else
+				return;
+		}
 		for (int i = 0; i < SECTION_COUNT; i++)
 		{
 			auto&& textLayout = m_impl->textLayouts[i];
 			setText(textLayout, tmp[i]);
 		}
+		update();
 	}
 }
 
-QString QCtmIPEdit::ip() const
+QString QCtmIPEdit::ipAddress() const
 {
 	QStringList ips;
 	for (auto&& textLayout : m_impl->textLayouts)
@@ -81,7 +136,7 @@ QTextLayout& QCtmIPEdit::textLayout(int pos) const
 	return m_impl->textLayouts[section];
 }
 
-QRect QCtmIPEdit::rectOfIpSection(int section)
+QRect QCtmIPEdit::rectOfIpSection(int section) const
 {
 	QStyleOptionFrame opt;
 	initStyleOption(&opt);
@@ -90,28 +145,37 @@ QRect QCtmIPEdit::rectOfIpSection(int section)
 	if (rect.isEmpty())
 		rect = QRect(QPoint(0, 0), this->rect().size());
 
-	int ipSectionWidth = opt.fontMetrics.width("000");
+	int ipSectionWidth = opt.fontMetrics.width(" 000 ");
 	return { rect.left() + (dotWidth * section) + section * ipSectionWidth , rect.top(), ipSectionWidth, rect.height() };
 }
 
-QRect QCtmIPEdit::boundRect(int section, const QRect& rect)
+QRect QCtmIPEdit::boundRect(int section, const QRect& rect) const
 {
 	auto&& layout = m_impl->textLayouts[section];
 	auto&& txt = layout.text();
 	QStyleOptionFrame opt;
 	initStyleOption(&opt);
-	auto w = opt.fontMetrics.width(txt);
-	return { rect.x() + (rect.width() - w) / 2, rect.y(), w, rect.height() };
+	auto br = layout.boundingRect();
+	return { qRound(rect.x() + (rect.width() - br.width()) / 2), qRound(rect.y() + (rect.height() - br.height())/2), qRound(br.width()), qRound(br.height()) };
 }
 
-void QCtmIPEdit::setText(QTextLayout& textLayout, const QString& text)
+bool QCtmIPEdit::setText(QTextLayout& textLayout, const QString& text)
 {
+	if (!text.isEmpty())
+	{
+		bool ok{ false };
+		auto num = text.toInt(&ok);
+		if (num < 0 || num > 255)
+			return false;
+	}
 	textLayout.setText(text);
 
 	QStyleOptionFrame opt;
 	initStyleOption(&opt);
 
 	m_impl->ascent = redoTextLayout(textLayout) - opt.fontMetrics.ascent();
+	emit editChanged();
+	return true;
 }
 
 void QCtmIPEdit::paintEvent(QPaintEvent* event)
@@ -128,9 +192,9 @@ void QCtmIPEdit::paintEvent(QPaintEvent* event)
 	{
 		auto rect = rectOfIpSection(i);
 		auto txtRect = boundRect(i, rect);
-		m_impl->textLayouts[i].draw(&p, txtRect.topLeft(), m_impl->fr, txtRect);
+		m_impl->textLayouts[i].draw(&p, txtRect.topLeft(), m_impl->selections[i], txtRect);
 		if (i != SECTION_COUNT - 1)
-			p.drawText(QRect(rect.x() + rect.width(), rect.y(), opt.fontMetrics.width('.'), rect.height()), ".");
+			p.drawText(QRect(rect.x() + rect.width(), txtRect.y(), opt.fontMetrics.width('.'), txtRect.height()), ".");
 	}
 	if (m_impl->cursorSwitch && hasFocus())
 	{
@@ -138,36 +202,49 @@ void QCtmIPEdit::paintEvent(QPaintEvent* event)
 		auto section = sectionOfCursorPosition(m_impl->cursorPosition);
 		section = section > 3 ? 3 : section;
 		auto rect = boundRect(section, rectOfIpSection(section));
-		txtLayout.drawCursor(&p, QPoint(rect.left(), m_impl->ascent), m_impl->cursorPosition % 4);
+		txtLayout.drawCursor(&p, QPoint(rect.left(), rect.y() + m_impl->ascent), m_impl->cursorPosition % 4);
 	}
 }
 
-void QCtmIPEdit::keyReleaseEvent(QKeyEvent* event)
+void QCtmIPEdit::keyPressEvent(QKeyEvent* event)
 {
 	if (event->key() >= Qt::Key_0 && event->key() <= Qt::Key_9)
 	{
+		if (hasSelection())
+		{
+			deleteSelectedText();
+		}
 		auto&& layout = textLayout(m_impl->cursorPosition);
 		auto&& txt = layout.text();
-		if (txt.size() <= m_impl->cursorPosition)
+		if (txt.size() <= m_impl->cursorPosition % SECTION_CURSOR_COUNT)
 		{
-			if (txt.size() < 3)
+			if (txt.size() < SECTION_CURSOR_COUNT - 1)
 			{
 				txt.append(event->text());
-				m_impl->cursorPosition++;
-				if (txt.size() == 3 && sectionOfCursorPosition(m_impl->cursorPosition) != 3)
+				if (setText(layout, txt))
+				{
 					m_impl->cursorPosition++;
+					if (txt.size() == SECTION_CURSOR_COUNT - 1
+						&& sectionOfCursorPosition(m_impl->cursorPosition) != SECTION_CURSOR_COUNT - 1)
+						m_impl->cursorPosition++;
+				}
 			}
 		}
-		else if (txt.size() < 3)
+		else if (txt.size() < SECTION_CURSOR_COUNT - 1)
 		{
-			txt.insert(m_impl->cursorPosition, event->text());
-			m_impl->cursorPosition++;
+			txt.insert(m_impl->cursorPosition % SECTION_CURSOR_COUNT, event->text());
+			if (setText(layout, txt))
+				m_impl->cursorPosition++;
 		}
-
-		setText(layout, txt);
 	}
 	else if (event->key() == Qt::Key_Backspace)
 	{
+		if (hasSelection())
+		{
+			deleteSelectedText();
+			update();
+			return;
+		}
 		auto&& layout = textLayout(m_impl->cursorPosition);
 		auto&& txt = layout.text();
 		if (m_impl->cursorPosition % SECTION_CURSOR_COUNT == 0)
@@ -195,25 +272,73 @@ void QCtmIPEdit::keyReleaseEvent(QKeyEvent* event)
 	}
 	else if (event->key() == Qt::Key_Left || event->key() == Qt::Key_Up)
 	{
-		if (m_impl->cursorPosition > 0)
+		if (!event->modifiers().testFlag(Qt::ShiftModifier) && hasSelection())
+		{
+			clearSelection();
+		}
+		else if (event->modifiers().testFlag(Qt::ShiftModifier))
+		{
+			auto start = m_impl->cursorPosition % SECTION_CURSOR_COUNT;
+			if (start == 0)
+				return;
+			auto section = sectionOfCursorPosition(m_impl->cursorPosition);
+			auto&& selection = m_impl->selections[section];
+			if (selection.isEmpty())
+			{
+				QTextLayout::FormatRange&& range = createFormatRange(m_impl->cursorPosition - 1
+					, m_impl->cursorPosition
+					, this->palette());
+				selection.push_back(range);
+			}
+			else
+			{
+				auto&& range = selection.front();
+				moveFormatRange(range, m_impl->cursorPosition, m_impl->cursorPosition - 1);
+			}
+			m_impl->cursorPosition--;
+		}
+		else if (m_impl->cursorPosition > 0)
 		{
 			m_impl->cursorPosition--;
 			auto section = sectionOfCursorPosition(m_impl->cursorPosition);
 			auto&& txtLayout = m_impl->textLayouts[section];
 			auto sectionMaxCursor = txtLayout.text().size() + (section)* SECTION_CURSOR_COUNT;
-			if(m_impl->cursorPosition > sectionMaxCursor)
-			m_impl->cursorPosition = sectionMaxCursor;
+			if (m_impl->cursorPosition > sectionMaxCursor)
+				m_impl->cursorPosition = sectionMaxCursor;
 		}
 	}
 	else if (event->key() == Qt::Key_Right || event->key() == Qt::Key_Down)
 	{
-		if (m_impl->cursorPosition < SECTION_CURSOR_COUNT * SECTION_COUNT)
+		if (!event->modifiers().testFlag(Qt::ShiftModifier) && hasSelection())
+		{
+			clearSelection();
+		}
+		else if (event->modifiers().testFlag(Qt::ShiftModifier))
+		{
+			auto start = m_impl->cursorPosition % SECTION_CURSOR_COUNT;
+			if (start == SECTION_CURSOR_COUNT - 1)
+				return;
+			auto section = sectionOfCursorPosition(m_impl->cursorPosition);
+			auto&& selection = m_impl->selections[section];
+			if (selection.isEmpty())
+			{
+				auto&& range = createFormatRange(m_impl->cursorPosition, m_impl->cursorPosition + 1, this->palette());
+				selection.push_back(range);
+			}
+			else
+			{
+				auto&& range = selection.front();
+				moveFormatRange(range, m_impl->cursorPosition, m_impl->cursorPosition + 1);
+			}
+			m_impl->cursorPosition++;
+		}
+		else if (m_impl->cursorPosition < SECTION_CURSOR_COUNT * SECTION_COUNT)
 		{
 			auto&& section = sectionOfCursorPosition(m_impl->cursorPosition);
 			auto&& txtLayout = m_impl->textLayouts[section];
-			if (m_impl->cursorPosition % 4 == txtLayout.text().size())
+			if (m_impl->cursorPosition % SECTION_CURSOR_COUNT == txtLayout.text().size())
 			{
-				if(section < 3)
+				if (section < SECTION_COUNT - 1)
 					m_impl->cursorPosition = (section + 1) * SECTION_CURSOR_COUNT;
 			}
 			else
@@ -222,6 +347,12 @@ void QCtmIPEdit::keyReleaseEvent(QKeyEvent* event)
 	}
 	else if (event->key() == Qt::Key_Delete)
 	{
+		if (hasSelection())
+		{
+			deleteSelectedText();
+			update();
+			return;
+		}
 		auto&& txtLayout = textLayout(m_impl->cursorPosition);
 		if (txtLayout.text().size() > m_impl->cursorPosition % 4)
 		{
@@ -232,13 +363,117 @@ void QCtmIPEdit::keyReleaseEvent(QKeyEvent* event)
 	else if (event->text() == ".")
 	{
 		auto section = sectionOfCursorPosition(m_impl->cursorPosition);
-		auto &&txtLayout = m_impl->textLayouts[section];
-		if (!txtLayout.text().isEmpty() && section < SECTION_COUNT - 1 &&txtLayout.text().size() == m_impl->cursorPosition % 4)
+		auto&& txtLayout = m_impl->textLayouts[section];
+		if (!txtLayout.text().isEmpty() && section < SECTION_COUNT - 1 && txtLayout.text().size() == m_impl->cursorPosition % 4)
 		{
 			m_impl->cursorPosition = (section + 1) * SECTION_CURSOR_COUNT;
 		}
 	}
+	else if (event->key() == Qt::Key_Escape)
+	{
+		clearSelection();
+	}
 	update();
+}
+
+void QCtmIPEdit::mouseDoubleClickEvent(QMouseEvent* event)
+{
+	if (event->button() == Qt::LeftButton && event->modifiers().testFlag(Qt::NoModifier))
+	{
+		clearSelection();
+		auto cursor = xToCursor(event->pos().x());
+		auto section = sectionOfCursorPosition(cursor);
+		QTextLayout::FormatRange range;
+		range.start = 0;
+		range.length = m_impl->textLayouts[section].text().size();
+		m_impl->cursorPosition = section * SECTION_CURSOR_COUNT + range.length;
+		range.format.setBackground(palette().brush(QPalette::Highlight));
+		range.format.setForeground(palette().brush(QPalette::HighlightedText));
+		m_impl->selections[section].push_back(range);
+		update();
+	}
+}
+
+void QCtmIPEdit::mousePressEvent(QMouseEvent* event)
+{
+	if (event->button() == Qt::LeftButton && event->modifiers().testFlag(Qt::NoModifier))
+	{
+		clearSelection();
+		m_impl->cursorPosition = xToCursor(event->pos().x());
+		m_impl->pressedSection = sectionOfCursorPosition(m_impl->cursorPosition);
+		m_impl->pressedCursor = m_impl->cursorPosition;
+		m_impl->pressed = true;
+		update();
+	}
+	else if (event->button() == Qt::LeftButton && event->modifiers().testFlag(Qt::ShiftModifier))
+	{
+		auto section = sectionOfCursorPosition(m_impl->cursorPosition);
+		auto pressedCursorPosition = xToCursor(event->pos().x());
+		if (section != sectionOfCursorPosition(pressedCursorPosition))
+			return;
+		auto&& selection = m_impl->selections[section];
+		if (selection.isEmpty())
+		{
+			auto&& range = createFormatRange(m_impl->cursorPosition, pressedCursorPosition, this->palette());
+			selection.push_back(range);
+		}
+		else
+		{
+			auto&& range = selection.front();
+			moveFormatRange(range, m_impl->cursorPosition, pressedCursorPosition);
+		}
+		m_impl->cursorPosition = pressedCursorPosition;
+		update();
+	}
+}
+
+void QCtmIPEdit::mouseMoveEvent(QMouseEvent* event)
+{
+	if (event->modifiers().testFlag(Qt::NoModifier) && m_impl->pressed)
+	{
+		auto movedCursorPosition = xToCursor(event->pos().x());
+		if (sectionOfCursorPosition(movedCursorPosition) != m_impl->pressedSection)
+			return;
+		auto&& selection = m_impl->selections[m_impl->pressedSection];
+		if (selection.isEmpty())
+		{
+			auto&& range = createFormatRange(m_impl->pressedCursor, movedCursorPosition, this->palette());
+			selection.push_back(range);
+			m_impl->cursorPosition = movedCursorPosition;
+		}
+		else
+		{
+			auto&& range = selection.front();
+			moveFormatRange(range, m_impl->cursorPosition, movedCursorPosition);
+			m_impl->cursorPosition = movedCursorPosition;
+		}
+		update();
+	}
+}
+
+void QCtmIPEdit::mouseReleaseEvent(QMouseEvent* event)
+{
+	if (event->button() == Qt::LeftButton && event->modifiers().testFlag(Qt::NoModifier))
+		m_impl->pressed = false;
+}
+
+void QCtmIPEdit::focusInEvent(QFocusEvent* event)
+{
+	m_impl->cursorSwitch = true;
+	m_impl->timer.start();
+	update();
+}
+
+void QCtmIPEdit::focusOutEvent(QFocusEvent* event)
+{
+	m_impl->cursorSwitch = false;
+	m_impl->timer.stop();
+	Qt::FocusReason reason = event->reason();
+	if (reason != Qt::ActiveWindowFocusReason &&
+		reason != Qt::PopupFocusReason)
+		clearSelection();
+	update();
+	emit editFinished();
 }
 
 QSize QCtmIPEdit::sizeHint() const
@@ -253,7 +488,7 @@ QSize QCtmIPEdit::minimumSizeHint() const
 	int left, top, right, bottom;
 	this->getContentsMargins(&left, &top, &right, &bottom);
 	int h = fm.height() + qMax(2 * m_impl->verticalMargin, fm.leading()) + top + bottom;
-	int w = fm.width("000.000.000.000") + left + right;
+	int w = fm.width(" 000 . 000 . 000 . 000 ") + left + right;
 	QStyleOptionFrame opt;
 	initStyleOption(&opt);
 	return (style()->sizeFromContents(QStyle::CT_LineEdit, &opt, QSize(w, h).
@@ -274,6 +509,127 @@ void QCtmIPEdit::initStyleOption(QStyleOptionFrame* option) const
 	if (m_impl->readOnly)
 		option->state |= QStyle::State_ReadOnly;
 	option->features = QStyleOptionFrame::None;
+}
+
+int QCtmIPEdit::xToCursor(int x) const
+{
+	for (int i = 0; i < SECTION_COUNT; i++)
+	{
+		auto&& rect = rectOfIpSection(i);
+		if (rect.contains(x, rect.y()))
+		{
+			auto bound = boundRect(i, rect);
+			x -= bound.x();
+			auto&& textLayout = m_impl->textLayouts[i];
+			return i * SECTION_CURSOR_COUNT + textLayout.lineAt(0).xToCursor(x);
+		}
+		else if (i == SECTION_COUNT - 1)
+		{
+			if (x > rect.right())
+			{
+				auto bound = boundRect(i, rect);
+				x -= bound.x();
+				auto&& textLayout = m_impl->textLayouts[i];
+				return i * SECTION_CURSOR_COUNT + textLayout.lineAt(0).xToCursor(x);
+			}
+		}
+	}
+	return 0;
+}
+
+void QCtmIPEdit::clearSelection()
+{
+	for (auto&& fr : m_impl->selections)
+	{
+		fr.clear();
+	}
+}
+
+void QCtmIPEdit::deleteSelectedText()
+{
+	for (int section = 0; section != SECTION_COUNT; section++)
+	{
+		auto fr = m_impl->selections[section];
+		if (!fr.isEmpty())
+		{
+			auto&& range = fr.front();
+			auto&& textLayout = m_impl->textLayouts[section];
+			setText(textLayout, textLayout.text().remove(range.start, range.length));
+			m_impl->cursorPosition = section * SECTION_CURSOR_COUNT + textLayout.text().size();
+		}
+	}
+	clearSelection();
+}
+
+bool QCtmIPEdit::hasSelection() const
+{
+	for (auto fr : m_impl->selections)
+	{
+		if (!fr.isEmpty())
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+void QCtmIPEdit::initActions()
+{
+	m_impl->copy = new QAction(tr("Copy"), this);
+	m_impl->copy->setShortcut(QKeySequence::Copy);
+	m_impl->copy->setShortcutContext(Qt::WidgetWithChildrenShortcut);
+	connect(m_impl->copy, &QAction::triggered, this, [=]() {
+		for (int section = 0; section < SECTION_COUNT; section++)
+		{
+			auto selection = m_impl->selections[section];
+			if (!selection.isEmpty())
+			{
+				auto&& range = selection.front();
+				auto text = m_impl->textLayouts[section].text().mid(range.start, range.length);
+				qApp->clipboard()->setText(text);
+				return;
+			}
+		}
+		});
+	m_impl->paste = new QAction(tr("Paste"), this);
+	m_impl->paste->setShortcut(QKeySequence::Paste);
+	m_impl->paste->setShortcutContext(Qt::WidgetWithChildrenShortcut);
+	connect(m_impl->paste, &QAction::triggered, this, [=]() {
+		if (qApp->clipboard()->text().isEmpty())
+			return;
+		if (qApp->clipboard()->text().split(".").size() == 4)
+		{
+			setIPAddress(qApp->clipboard()->text());
+			return;
+		}
+		auto&& section = sectionOfCursorPosition(m_impl->cursorPosition);
+		auto&& txtLayout = m_impl->textLayouts[section];
+		auto&& text = txtLayout.text();
+		auto&& selection = m_impl->selections[section];
+		auto local = m_impl->cursorPosition % SECTION_CURSOR_COUNT;
+		if (selection.isEmpty())
+		{
+			text.insert(local, qApp->clipboard()->text());
+			if (text.size() > SECTION_CURSOR_COUNT - 1)
+				return;
+			setText(txtLayout, text);
+			m_impl->cursorPosition = section * SECTION_CURSOR_COUNT + local + 1;
+		}
+		else
+		{
+			auto range = selection.front();
+			text.replace(range.start, range.length, qApp->clipboard()->text());
+			if (text.size() > SECTION_CURSOR_COUNT - 1)
+				return;
+			setText(txtLayout, text);
+			m_impl->cursorPosition = section * SECTION_CURSOR_COUNT + range.start + range.length;
+		}
+		clearSelection();
+		update();
+		});
+
+	addAction(m_impl->copy);
+	addAction(m_impl->paste);
 }
 
 int QCtmIPEdit::redoTextLayout(QTextLayout& textLayout) const
@@ -300,4 +656,32 @@ void QCtmIPEdit::init()
 		textLayout.createLine();
 		textLayout.endLayout();
 	}
+}
+
+void QCtmIPEdit::onCustomContextMenuRequested(const QPoint& pos)
+{
+	QMenu menu(this);
+	bool selected = false;
+	for (auto selection : m_impl->selections)
+	{
+		if (!selection.isEmpty())
+		{
+			if (selection.front().length != 0)
+			{
+				selected = true;
+				break;
+			}
+		}
+	}
+	if (selected)
+	{
+		menu.addAction(m_impl->copy);
+	}
+	if (!qApp->clipboard()->text().isEmpty())
+	{
+		menu.addAction(m_impl->paste);
+	}
+
+	if(!menu.isEmpty())
+		menu.exec(this->mapToGlobal(pos));
 }
--- src/QCustomUi/QCtmIPEdit.h
+++ src/QCustomUi/QCtmIPEdit.h
@@ -7,7 +7,6 @@
 #include <memory>
 
 class QStyleOptionFrame;
-class QTextLayout;
 
 class QCUSTOMUI_EXPORT QCtmIPEdit : public QWidget
 {
@@ -17,25 +16,41 @@ public:
 	QCtmIPEdit(QWidget *parent);
 	~QCtmIPEdit();
 
-	void setIP(const QString& ip);
-	QString ip() const;
+	void setIPAddress(const QString& ip);
+	QString ipAddress() const;
+
+signals:
+	void editChanged();
+	void editFinished();
 
 protected:
 	void paintEvent(QPaintEvent* event) override;
-	void keyReleaseEvent(QKeyEvent* event) override;
+	void keyPressEvent(QKeyEvent* event) override;
+	void mouseDoubleClickEvent(QMouseEvent* event) override;
+	void mousePressEvent(QMouseEvent* event) override;
+	void mouseMoveEvent(QMouseEvent* event) override;
+	void mouseReleaseEvent(QMouseEvent* event) override;
+	void focusInEvent(QFocusEvent* event) override;
+	void focusOutEvent(QFocusEvent* event) override;
 	QSize sizeHint() const override;
 	QSize minimumSizeHint() const override;
-	void initStyleOption(QStyleOptionFrame* option) const;
 
 private:
 	int sectionOfCursorPosition(int position) const;
 	QTextLayout& textLayout(int pos) const;
-	QRect rectOfIpSection(int section);
-	QRect boundRect(int section, const QRect& rect);
-	void setText(QTextLayout& textLayout, const QString& text);
+	QRect rectOfIpSection(int section) const;
+	QRect boundRect(int section, const QRect& rect) const;
+	bool setText(QTextLayout& textLayout, const QString& text);
 	int redoTextLayout(QTextLayout& textLayout) const;
+	void initStyleOption(QStyleOptionFrame* option) const;
+	int xToCursor(int x) const;
+	void clearSelection();
+	void deleteSelectedText();
+	bool hasSelection() const;
+	void initActions();
 private slots:
 	void init();
+	void onCustomContextMenuRequested(const QPoint& pos);
 private:
 	struct Impl;
 	std::unique_ptr<Impl> m_impl;
