09178841e5f0dd7b615d480501eb0613b74eaa85
lilong <634916105@qq.com>
2019-08-27 11:15:10 +0800

    修改部分样式

--- src/QCustomUi/QCtmIPAddressEdit.h
+++ src/QCustomUi/QCtmIPAddressEdit.h
@@ -1,3 +1,9 @@
+/*!***************************************************************************
+*  Copyright (C) 2019 lilong(DLC)
+*  This file is part of the QCustomUi
+*  Contact: 634916105@qq.com
+*  License: LGPL
+*****************************************************************************/
 #pragma once
 
 #include "qcustomui_global.h"
