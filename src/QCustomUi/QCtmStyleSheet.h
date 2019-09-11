82376b155ee59ef77b104af361853d8fe0bb5c3c
lilong <634916105@qq.com>
2019-09-11 17:07:07 +0800

    将资源移动到QCustomUi

---
+++ src/QCustomUi/QCtmStyleSheet.h
@@ -0,0 +1,18 @@
+/*!***************************************************************************
+*  Copyright (C) 2019 lilong(DLC)
+*  This file is part of the QCustomUi
+*  Contact: 634916105@qq.com
+*  License: LGPL
+*****************************************************************************/
+#pragma once
+
+#include "qcustomui_global.h"
+
+#include <QString>
+
+class QCUSTOMUI_EXPORT QCtmStyleSheet
+{
+public:
+	static QString defaultStyleSheet();
+};
+
