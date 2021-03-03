3fe61a9556ed00eaaea5b66dfb2cc2f5e2807c0c
lilong <634916105@qq.com>
2021-03-03 15:46:16 +0800

    update version to 1.2.0

--- README.md
+++ README.md
@@ -2,8 +2,18 @@
 
 文档：http://llongww.gitee.io/qcustomuidocument
 
-**[1.1.3]**
-> 1.QCtmStageProgressBar bugfix：设置文字隐藏无效，文字隐藏后水平的进度条最小高度不正确，垂直的进度条显示异常。
+**[1.2.0]**
+```
+1.添加QCtmLoadingDialog。
+```
 
+**[1.1.3]**
+```
+1.QCtmStageProgressBar bugfix：设置文字隐藏无效，
+  文字隐藏后水平的进度条最小高度不正确，垂直的进度条显示异常。
+```
 **[1.1.2]**
-> 1.添加QCtmClassifyTreeView，显示类似于设置了IconMode的QListView，但添加了类似树形结构的分组功能。
+```
+1.添加QCtmClassifyTreeView，显示类似于设置了
+  IconMode的QListView，但添加了类似树形结构的分组功能。
+```
--- src/QCustomUi/QCtmLoadingDialog.cpp
+++ src/QCustomUi/QCtmLoadingDialog.cpp
@@ -1,4 +1,23 @@
-﻿#include "QCtmLoadingDialog.h"
+﻿/*********************************************************************************
+**                                                                              **
+**  Copyright (C) 2019-2020 LiLong                                              **
+**  This file is part of QCustomUi.                                             **
+**                                                                              **
+**  QCustomUi is free software: you can redistribute it and/or modify           **
+**  it under the terms of the GNU Lesser General Public License as published by **
+**  the Free Software Foundation, either version 3 of the License, or           **
+**  (at your option) any later version.                                         **
+**                                                                              **
+**  QCustomUi is distributed in the hope that it will be useful,                **
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of              **
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               **
+**  GNU Lesser General Public License for more details.                         **
+**                                                                              **
+**  You should have received a copy of the GNU Lesser General Public License    **
+**  along with QCustomUi.  If not, see <https://www.gnu.org/licenses/>.         **
+**********************************************************************************/
+
+#include "QCtmLoadingDialog.h"
 #include "ui_QCtmLoadingDialog.h"
 
 #include <QApplication>
@@ -11,7 +30,18 @@ struct QCtmLoadingDialog::Impl
     ~Impl() { delete ui; }
 };
 
+/*!
+    \class      QCtmLoadingDialog
+    \brief      等待加载窗口.
+    \inherits   QDialog
+    \ingroup    QCustomUi
+    \inmodule   QCustomUi
+    \inheaderfile QCtmLoadingDialog.h
+*/
 
+/*!
+    \brief      构造函数，在 \a parent 的顶层窗口上覆盖加载窗口.
+*/
 QCtmLoadingDialog::QCtmLoadingDialog(QWidget* parent)
     : QDialog(parent)
     , m_impl(std::make_unique<Impl>())
@@ -24,10 +54,16 @@ QCtmLoadingDialog::QCtmLoadingDialog(QWidget* parent)
     m_impl->ui->label->setMovie(m_impl->movie);
 }
 
+/*!
+    \brief      析构该窗口对象.
+*/
 QCtmLoadingDialog::~QCtmLoadingDialog()
 {
 }
 
+/*!
+    \reimp
+*/
 void QCtmLoadingDialog::showEvent(QShowEvent* e)
 {
     auto pw = this->parentWidget();
