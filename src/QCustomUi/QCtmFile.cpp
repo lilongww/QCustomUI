896c35fd7d3a45d6167d8d6e3996f46d2d355ae2
lilong <634916105@qq.com>
2021-04-28 15:09:55 +0800

    删除冗余文件 添加QCtmFile

---
+++ src/QCustomUi/QCtmFile.cpp
@@ -0,0 +1,57 @@
+﻿/*********************************************************************************
+**                                                                              **
+**  Copyright (C) 2019-2020 LiLong                                              **
+**  This file is part of QCustomUi.                                             **
+**                                                                              **
+**  QCustomUi is free software: you can redistribute it and/or modify           **
+**  it under the terms of the GNU Lesser General Public License as published by **
+**  the Free Software Foundation, either version 3 of the License, or           **
+**  (at your option) any later version.                                         **
+**                                                                              **
+**  QCustomUi is distributed in the hope that it will be useful,                **
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of              **
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               **
+**  GNU Lesser General Public License for more details.                         **
+**                                                                              **
+**  You should have received a copy of the GNU Lesser General Public License    **
+**  along with QCustomUi.  If not, see <https://www.gnu.org/licenses/>.         **
+**********************************************************************************/
+#include "QCtmFile.h"
+
+/*!
+    \class      QCtmFile
+    \brief      文件操作类，提供文件读写的便捷方法.
+    \inherits   QFile
+    \ingroup    QCustomUi
+    \inmodule   QCustomUi
+    \inheaderfile QCtmFile.h
+*/
+
+/*!
+    \brief      加载文件 \a file, 可设置文件打开模式 \a mode, 如果读取文件失败返回 std::nullopt，读取成功返回文件内容.
+    \sa         save
+*/
+std::optional<QByteArray> QCtmFile::load(const QString& file, QIODevice::OpenMode mode)
+{
+    if (!mode.testFlag(QIODevice::ReadOnly))
+        return std::nullopt;
+    QFile f(file);
+    if (!f.open(mode))
+        return std::nullopt;
+    return f.readAll();
+}
+
+/*!
+    \brief      保存内容 \a content 到文件 \a file 中，可设置文件打开模式 \a mode, 如果保存成功则返回true，否则返回false.
+    \sa         load
+*/
+bool QCtmFile::save(const QString& file, const QByteArray& content, QIODevice::OpenMode mode /*= QIODevice::WriteOnly*/)
+{
+    if (!mode.testFlag(QIODevice::WriteOnly))
+        return false;
+    QFile f(file);
+    if (!f.open(mode))
+        return false;
+    f.write(content);
+    return true;
+}
---
+++ src/QCustomUi/QCtmFile.h
@@ -0,0 +1,34 @@
+﻿/*********************************************************************************
+**                                                                              **
+**  Copyright (C) 2019-2020 LiLong                                              **
+**  This file is part of QCustomUi.                                             **
+**                                                                              **
+**  QCustomUi is free software: you can redistribute it and/or modify           **
+**  it under the terms of the GNU Lesser General Public License as published by **
+**  the Free Software Foundation, either version 3 of the License, or           **
+**  (at your option) any later version.                                         **
+**                                                                              **
+**  QCustomUi is distributed in the hope that it will be useful,                **
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of              **
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               **
+**  GNU Lesser General Public License for more details.                         **
+**                                                                              **
+**  You should have received a copy of the GNU Lesser General Public License    **
+**  along with QCustomUi.  If not, see <https://www.gnu.org/licenses/>.         **
+**********************************************************************************/
+#pragma once
+
+#include "qcustomui_global.h"
+
+#include <QFile>
+
+#include <optional>
+
+class QCUSTOMUI_EXPORT QCtmFile : public QFile
+{
+public:
+    using QFile::QFile;
+
+    static std::optional<QByteArray> load(const QString& file, QIODevice::OpenMode mode = QIODevice::ReadOnly);
+    static bool save(const QString& file, const QByteArray& content, QIODevice::OpenMode mode = QIODevice::WriteOnly);
+};
