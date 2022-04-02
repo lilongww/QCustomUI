553c44cb73e5ef78f6c62e08a47a94126770bc40
lilong <634916105@qq.com>
2022-04-02 10:35:11 +0800

    add license

--- src/OpenVisa/Private/RawSocket.h
+++ src/OpenVisa/Private/RawSocket.h
@@ -1,4 +1,22 @@
-﻿#pragma once
+﻿/*********************************************************************************
+**                                                                              **
+**  Copyright (C) 2022 LiLong                                                   **
+**  This file is part of OpenVisa.                                              **
+**                                                                              **
+**  OpenVisa is free software: you can redistribute it and/or modify            **
+**  it under the terms of the GNU Lesser General Public License as published by **
+**  the Free Software Foundation, either version 3 of the License, or           **
+**  (at your option) any later version.                                         **
+**                                                                              **
+**  OpenVisa is distributed in the hope that it will be useful,                 **
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of              **
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               **
+**  GNU Lesser General Public License for more details.                         **
+**                                                                              **
+**  You should have received a copy of the GNU Lesser General Public License    **
+**  along with OpenVisa.  If not, see <https://www.gnu.org/licenses/>.          **
+**********************************************************************************/
+#pragma once
 
 #include "../Address.h"
 #include "IOBase.h"
--- src/OpenVisa/Private/UsbTmcProtocol.h
+++ src/OpenVisa/Private/UsbTmcProtocol.h
@@ -1,4 +1,22 @@
-﻿#pragma once
+﻿/*********************************************************************************
+**                                                                              **
+**  Copyright (C) 2022 LiLong                                                   **
+**  This file is part of OpenVisa.                                              **
+**                                                                              **
+**  OpenVisa is free software: you can redistribute it and/or modify            **
+**  it under the terms of the GNU Lesser General Public License as published by **
+**  the Free Software Foundation, either version 3 of the License, or           **
+**  (at your option) any later version.                                         **
+**                                                                              **
+**  OpenVisa is distributed in the hope that it will be useful,                 **
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of              **
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               **
+**  GNU Lesser General Public License for more details.                         **
+**                                                                              **
+**  You should have received a copy of the GNU Lesser General Public License    **
+**  along with OpenVisa.  If not, see <https://www.gnu.org/licenses/>.          **
+**********************************************************************************/
+#pragma once
 
 namespace OpenVisa
 {
@@ -155,7 +173,7 @@ public:
     {
         if (buffer.size() < sizeof(Header))
             return false;
-        auto header   = reinterpret_cast<const Header*>(buffer.c_str());
+        auto header = reinterpret_cast<const Header*>(buffer.c_str());
 
         if (header->m_transferSize + sizeof(Header) > buffer.size())
             return false;
