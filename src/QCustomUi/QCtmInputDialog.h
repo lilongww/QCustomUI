0fdad8a95c9232241142355617358ed444d65104
lilong <634916105@qq.com>
2022-10-14 15:09:48 +0800

    部分头文件无唯一包含和版权声明

--- src/QCustomUi/QCtmInputDialog.h
+++ src/QCustomUi/QCtmInputDialog.h
@@ -16,6 +16,7 @@
 **  You should have received a copy of the GNU Lesser General Public License    **
 **  along with QCustomUi.  If not, see <https://www.gnu.org/licenses/>.         **
 **********************************************************************************/
+#pragma once
 
 #include "qcustomui_global.h"
 
