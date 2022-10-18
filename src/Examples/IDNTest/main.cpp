d9557ed4e273966cf3aa4fcaf1701d337ef976f3
lilong <634916105@qq.com>
2022-10-18 09:18:52 +0800

    update version

--- src/Examples/IDNTest/main.cpp
+++ src/Examples/IDNTest/main.cpp
@@ -9,9 +9,9 @@ int main(int argc, char* argv[])
         OpenVisa::Object obj;
         // obj.connect(OpenVisa::Address<OpenVisa::AddressType::HiSLIP>("192.168.2.111"));
         //  obj.connect(OpenVisa::Address<OpenVisa::AddressType::VXI11>("192.168.2.111", "inst0"));
-        obj.connect(OpenVisa::Address<OpenVisa::AddressType::USB>(0xF4EC, 0x1102, "SDG2XFBQ6R2965"));
-        for (int i = 0; i < 1024; i++)
-            std::cout << obj.query("*IDN?");
+        obj.connect(OpenVisa::Address<OpenVisa::AddressType::USB>(0x1AB1, 0x0514, "DS7A241200172"));
+        //for (int i = 0; i < 1024; i++)
+        //    std::cout << obj.query("*IDN?");
         // auto buffer = obj.query("MMEMory:DATA? 'C:\\R_S\\Instr\\install\\FoxitReader_Setup.exe'");
         // auto buffer = obj.query("MMEMory:DATA? 'C:\\Program Files (x86)\\Rohde-Schwarz\\RsVisa\\VisaManual.pdf'");
         // std::cout << buffer.size();
@@ -19,7 +19,7 @@ int main(int argc, char* argv[])
         // auto str = std::to_string(1024 * 1024);
         // obj.send("MMEM:CDIR 'C:\\R_S'");
         // obj.send("MMEMory:DATA '1.txt',#{}{}{}", str.size(), str, buffer);
-        // std::cout << obj.query("*IDN?");
+         std::cout << obj.query("*IDN?");
     }
     catch (const std::exception& e)
     {
