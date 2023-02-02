c72176ed7bdda64c30ad2c0e404cca41155dbc7d
lilong <634916105@qq.com>
2023-02-02 17:01:07 +0800

    脚本错误

--- cmake/ExportHelper.cmake
+++ cmake/ExportHelper.cmake
@@ -1,10 +1,10 @@
-function(export_config target_name install_dir)
+function(export_config target_name installdir includedir)
     include(CMakePackageConfigHelpers)
 
     configure_package_config_file("${PROJECT_SOURCE_DIR}/cmake/${target_name}Config.cmake.in"
         "${CMAKE_CURRENT_BINARY_DIR}/${target_name}Config.cmake"
-        INSTALL_DESTINATION "${install_dir}/cmake/"
-        PATH_VARS install_dir)
+        INSTALL_DESTINATION "${installdir}/cmake/"
+        PATH_VARS installdir includedir)
 
     write_basic_package_version_file(
         "${CMAKE_CURRENT_BINARY_DIR}/${target_name}ConfigVersion.cmake"
@@ -12,7 +12,7 @@ function(export_config target_name install_dir)
         COMPATIBILITY AnyNewerVersion)
 
     install(FILES "${CMAKE_CURRENT_BINARY_DIR}/${target_name}ConfigVersion.cmake"
-        "${CMAKE_CURRENT_BINARY_DIR}/${target_name}Config.cmake" DESTINATION "${install_dir}/cmake/")
+        "${CMAKE_CURRENT_BINARY_DIR}/${target_name}Config.cmake" DESTINATION "${installdir}/cmake/")
     set(CMAKE_EXPORT_PACKAGE_REGISTRY ON)
     export(PACKAGE ${target_name})
 endfunction()
@@ -25,7 +25,5 @@ function(export_install_target target_name includedir libdir bindir)
         PUBLIC_HEADER DESTINATION ${includedir}/${target_name})
 
     install(EXPORT ${target_name} FILE "${target_name}Targets.cmake" DESTINATION "${CMAKE_CURRENT_BINARY_DIR}/cmake")
-    file(GLOB _targets "${CMAKE_CURRENT_BINARY_DIR}/cmake/${target_name}Targets*.cmake")
-    install(FILES ${_targets} DESTINATION "${libdir}/cmake")
-    unset(_targets)
+    install(DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/cmake" DESTINATION "${libdir}" FILES_MATCHING PATTERN "*.cmake")
 endfunction()
