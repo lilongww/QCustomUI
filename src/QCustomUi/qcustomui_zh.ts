0523c0eb3e91cc9b993e64dcff85a0ec77613ec8
lilong <634916105@qq.com>
2023-05-30 10:53:12 +0800

    更新中文翻译

--- src/QCustomUi/qcustomui_zh.ts
+++ src/QCustomUi/qcustomui_zh.ts
@@ -2,35 +2,67 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="zh_CN">
 <context>
+    <name>QCtmDigitKeyboard</name>
+    <message>
+        <location filename="QCtmDigitKeyboard.ui" line="32"/>
+        <source>←</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="QCtmDigitKeyboard.cpp" line="604"/>
+        <location filename="QCtmDigitKeyboard.cpp" line="612"/>
+        <source>%1%2 - %3%4</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="QCtmDigitKeyboard.cpp" line="618"/>
+        <source>Warning</source>
+        <translation>警告</translation>
+    </message>
+    <message>
+        <location filename="QCtmDigitKeyboard.cpp" line="618"/>
+        <source>Value out of range: %1</source>
+        <translation>数值超出范围：%1</translation>
+    </message>
+</context>
+<context>
     <name>QCtmIPAddressEdit</name>
     <message>
-        <location filename="QCtmIPAddressEdit.cpp" line="594"/>
+        <location filename="QCtmIPAddressEdit.cpp" line="772"/>
         <source>Copy</source>
         <translation>复制</translation>
     </message>
     <message>
-        <location filename="QCtmIPAddressEdit.cpp" line="610"/>
+        <location filename="QCtmIPAddressEdit.cpp" line="792"/>
         <source>Paste</source>
         <translation>粘贴</translation>
     </message>
 </context>
 <context>
+    <name>QCtmLoadingDialog</name>
+    <message>
+        <location filename="QCtmLoadingDialog.ui" line="14"/>
+        <source>LoadingDialog</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
     <name>QCtmLogModel</name>
     <message>
-        <location filename="QCtmLogModel.cpp" line="33"/>
-        <location filename="QCtmLogModel.cpp" line="229"/>
+        <location filename="QCtmLogModel.cpp" line="65"/>
+        <location filename="QCtmLogModel.cpp" line="307"/>
         <source>Lv</source>
         <translation>等级</translation>
     </message>
     <message>
-        <location filename="QCtmLogModel.cpp" line="33"/>
-        <location filename="QCtmLogModel.cpp" line="229"/>
+        <location filename="QCtmLogModel.cpp" line="65"/>
+        <location filename="QCtmLogModel.cpp" line="307"/>
         <source>DateTime</source>
         <translation>时间</translation>
     </message>
     <message>
-        <location filename="QCtmLogModel.cpp" line="33"/>
-        <location filename="QCtmLogModel.cpp" line="229"/>
+        <location filename="QCtmLogModel.cpp" line="65"/>
+        <location filename="QCtmLogModel.cpp" line="307"/>
         <source>Description</source>
         <translation>描述</translation>
     </message>
@@ -38,30 +70,63 @@
 <context>
     <name>QCtmLogWidget</name>
     <message>
-        <location filename="QCtmLogWidget.cpp" line="73"/>
-        <location filename="QCtmLogWidget.cpp" line="221"/>
+        <location filename="QCtmLogWidget.cpp" line="139"/>
+        <location filename="QCtmLogWidget.cpp" line="300"/>
         <source>Error %1</source>
         <translation>错误 %1</translation>
     </message>
     <message>
-        <location filename="QCtmLogWidget.cpp" line="77"/>
-        <location filename="QCtmLogWidget.cpp" line="222"/>
+        <location filename="QCtmLogWidget.cpp" line="143"/>
+        <location filename="QCtmLogWidget.cpp" line="301"/>
         <source>Warn %1</source>
         <translation>警告 %1</translation>
     </message>
     <message>
-        <location filename="QCtmLogWidget.cpp" line="81"/>
-        <location filename="QCtmLogWidget.cpp" line="223"/>
+        <location filename="QCtmLogWidget.cpp" line="147"/>
+        <location filename="QCtmLogWidget.cpp" line="302"/>
         <source>Info %1</source>
         <translation>信息 %1</translation>
     </message>
 </context>
 <context>
-    <name>QCtmMainWindow</name>
+    <name>QCtmMessageTipView</name>
     <message>
-        <location filename="QCtmMainWindow.ui" line="14"/>
-        <source>Form</source>
-        <translation></translation>
+        <location filename="QCtmMessageTipView.cpp" line="105"/>
+        <source>Clear All</source>
+        <translation>清空</translation>
+    </message>
+</context>
+<context>
+    <name>QCtmRecentModel</name>
+    <message>
+        <location filename="QCtmRecentModel.cpp" line="106"/>
+        <source>Top</source>
+        <translation>已固定</translation>
+    </message>
+    <message>
+        <location filename="QCtmRecentModel.cpp" line="108"/>
+        <source>Today</source>
+        <translation>今天</translation>
+    </message>
+    <message>
+        <location filename="QCtmRecentModel.cpp" line="110"/>
+        <source>Yesterday</source>
+        <translation>昨天</translation>
+    </message>
+    <message>
+        <location filename="QCtmRecentModel.cpp" line="112"/>
+        <source>This Week</source>
+        <translation>本周</translation>
+    </message>
+    <message>
+        <location filename="QCtmRecentModel.cpp" line="114"/>
+        <source>This Month</source>
+        <translation>本月</translation>
+    </message>
+    <message>
+        <location filename="QCtmRecentModel.cpp" line="116"/>
+        <source>Older</source>
+        <translation>更早</translation>
     </message>
 </context>
 <context>
