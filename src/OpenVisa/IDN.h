03d2d72147e60ece2926e521a8857760fbd5707a
lilong <634916105@qq.com>
2023-12-06 16:53:50 +0800

    添加IDN类型，解析IDN返回值，规整示例名称

---
+++ src/OpenVisa/IDN.h
@@ -0,0 +1,45 @@
+﻿/*********************************************************************************
+**                                                                              **
+**  Copyright (C) 2022-2023 LiLong                                              **
+**  This file is part of OpenVisa.                                              **
+**                                                                              **
+**  OpenVisa is free software: you can redistribute it and/or modify            **
+**  it under the terms of the GNU Lesser General Public License as published by **
+**  the Free Software Foundation, either version 3 of the License, or           **
+**  (at your option) any later version.                                         **
+**                                                                              **
+**  OpenVisa is distributed in the hope that it will be useful,                 **
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of              **
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               **
+**  GNU Lesser General Public License for more details.                         **
+**                                                                              **
+**  You should have received a copy of the GNU Lesser General Public License    **
+**  along with OpenVisa.  If not, see <https://www.gnu.org/licenses/>.          **
+**********************************************************************************/
+#pragma once
+
+#include <ranges>
+#include <string>
+#include <vector>
+
+namespace OpenVisa
+{
+class IDN
+{
+public:
+    inline IDN(const std::string& source)
+    {
+        auto view =
+            std::views::split(source, ',') | std::views::transform([](const auto& view) { return std::string(view.begin(), view.end()); });
+        m_datas = std::vector<std::string>(view.begin(), view.end());
+    }
+    inline ~IDN() {}
+    inline const std::string& manufacturer() const { return m_datas[0]; }
+    inline const std::string& model() const { return m_datas[1]; }
+    inline const std::string& serialNumber() const { return m_datas[2]; }
+    inline const std::string& firmwareRevision() const { return m_datas[3]; }
+
+private:
+    std::vector<std::string> m_datas;
+};
+} // namespace OpenVisa
---
+++ src/Tests/IDNTest/main.cpp
@@ -0,0 +1,29 @@
+﻿#include <OpenVisa/IDN.h>
+#include <gtest/gtest.h>
+
+constexpr auto FullIDN  = "Siglent Technologies,SDS5104X,SDS5XDAD2R0160,4.6.0.8.7R1";
+constexpr auto EmptyIDN = "Siglent Technologies,SDS5104X,,0";
+
+TEST(AdlTest, bool)
+{
+    {
+        OpenVisa::IDN idn(FullIDN);
+        EXPECT_EQ(idn.manufacturer(), "Siglent Technologies");
+        EXPECT_EQ(idn.model(), "SDS5104X");
+        EXPECT_EQ(idn.serialNumber(), "SDS5XDAD2R0160");
+        EXPECT_EQ(idn.firmwareRevision(), "4.6.0.8.7R1");
+    }
+    {
+        OpenVisa::IDN idn(EmptyIDN);
+        EXPECT_EQ(idn.manufacturer(), "Siglent Technologies");
+        EXPECT_EQ(idn.model(), "SDS5104X");
+        EXPECT_TRUE(idn.serialNumber().empty());
+        EXPECT_EQ(idn.firmwareRevision(), "0");
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
