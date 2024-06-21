a05c39234af0cca6ec4c47d573cdb78d9f876def
lilong <634916105@qq.com>
2024-06-21 11:34:22 +0800

    添加ulonglong spinbox

---
+++ src/QCustomUi/QCtmULongLongSpinBox.h
@@ -0,0 +1,80 @@
+﻿/*********************************************************************************
+**                                                                              **
+**  Copyright (C) 2019-2024 LiLong                                              **
+**  This file is part of QCustomUi.                                             **
+**                                                                              **
+**  QCustomUi is free software: you can redistribute it and/or modify           **
+**  it under the terms of the GNU Lesser General Public License as published by **
+**  the Free Software Foundation, either version 3 of the License, or           **
+**  (at your option) any later version.                                         **
+**                                                                              **
+**  QCustomUi is distributed in the hope that it will be useful,                **
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of              **
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               **
+**  GNU Lesser General Public License for more details.                         **
+**                                                                              **
+**  You should have received a copy of the GNU Lesser General Public License    **
+**  along with QCustomUi.  If not, see <https://www.gnu.org/licenses/>.         **
+**********************************************************************************/
+#pragma once
+
+#include "qcustomui_global.h"
+
+#include <QAbstractSpinBox>
+#include <qtwidgetsglobal.h>
+
+class QCtmULongLongSpinBoxPrivate;
+class QCUSTOMUI_EXPORT QCtmULongLongSpinBox : public QAbstractSpinBox
+{
+    Q_OBJECT
+
+    Q_PROPERTY(QString suffix READ suffix WRITE setSuffix)
+    Q_PROPERTY(QString prefix READ prefix WRITE setPrefix)
+    Q_PROPERTY(QString cleanText READ cleanText)
+    Q_PROPERTY(qulonglong minimum READ minimum WRITE setMinimum)
+    Q_PROPERTY(qulonglong maximum READ maximum WRITE setMaximum)
+    Q_PROPERTY(qulonglong singleStep READ singleStep WRITE setSingleStep)
+    Q_PROPERTY(StepType stepType READ stepType WRITE setStepType)
+    Q_PROPERTY(qulonglong value READ value WRITE setValue NOTIFY valueChanged USER true)
+    Q_PROPERTY(int displayIntegerBase READ displayIntegerBase WRITE setDisplayIntegerBase)
+
+public:
+    explicit QCtmULongLongSpinBox(QWidget* parent = nullptr);
+    ~QCtmULongLongSpinBox();
+    qulonglong value() const;
+    void setPrefix(const QString& prefix);
+    QString prefix() const;
+    void setSuffix(const QString& suffix);
+    QString suffix() const;
+    QString cleanText() const;
+    void setSingleStep(qulonglong value);
+    qulonglong singleStep() const;
+    void setMinimum(qulonglong minimum);
+    qulonglong minimum() const;
+    void setMaximum(qulonglong maximum);
+    qulonglong maximum() const;
+    void setRange(qulonglong minimum, qulonglong maximum);
+    void setStepType(QAbstractSpinBox::StepType stepType);
+    StepType stepType() const;
+    void setDisplayIntegerBase(int base);
+    int displayIntegerBase() const;
+public slots:
+    void setValue(qulonglong value);
+
+signals:
+    void valueChanged(qulonglong);
+    void textChanged(const QString&);
+
+protected:
+    virtual QString textFromValue(qulonglong val) const;
+    virtual qulonglong valueFromText(const QString& text) const;
+    QValidator::State validate(QString& input, int& pos) const override;
+    void fixup(QString& str) const override;
+    QAbstractSpinBox::StepEnabled stepEnabled() const override;
+    void stepBy(int steps) override;
+    bool event(QEvent* event) override;
+
+private:
+    Q_DISABLE_COPY(QCtmULongLongSpinBox)
+    Q_DECLARE_PRIVATE(QCtmULongLongSpinBox)
+};
