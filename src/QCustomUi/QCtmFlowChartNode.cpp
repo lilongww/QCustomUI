fd6d71fb15f7c002fddd5681735856dac7870844
lilong <634916105@qq.com>
2024-11-15 15:38:58 +0800

    暂存

---
+++ src/QCustomUi/QCtmFlowChartNode.cpp
@@ -0,0 +1,41 @@
+﻿/*********************************************************************************
+**                                                                              **
+**  Copyright (C) 2019-2024 LiLong                                              **
+**  This file is part of QCustomUi.                                             **
+**                                                                              **
+**  QCustomUi is free software: you can redistribute it and/or modify           **
+**  it under the terms of the GNU Lesser General Public License as published by **
+**  the Free Software Foundation, either version 3 of the License, or           **
+**  (at your option) any later version.                                         **
+**                                                                              **
+**  QCustomUi is distributed in the hope that it will be useful,                **
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of              **
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               **
+**  GNU Lesser General Public License for more details.                         **
+**                                                                              **
+**  You should have received a copy of the GNU Lesser General Public License    **
+**  along with QCustomUi.  If not, see <https://www.gnu.org/licenses/>.         **
+**********************************************************************************/
+
+#include "QCtmFlowChartNode.h"
+
+struct QCtmFlowChartNode::Impl
+{
+    std::vector<QCtmFlowChartNode> nexts;
+    QString text;
+};
+
+QCtmFlowChartNode::QCtmFlowChartNode() : m_impl(std::make_unique<Impl>()) {}
+
+QCtmFlowChartNode::~QCtmFlowChartNode() {}
+
+const std::vector<QCtmFlowChartNode>& QCtmFlowChartNode::nexts() const { return m_impl->nexts; }
+
+void QCtmFlowChartNode::setText(const QString& text)
+{
+    m_impl->text = text;
+    emit textChanged(text);
+    update();
+}
+
+QString QCtmFlowChartNode::text() const { return m_impl->text; }
---
+++ src/QCustomUi/QCtmFlowChartNode.h
@@ -0,0 +1,59 @@
+﻿/*********************************************************************************
+**                                                                              **
+**  Copyright (C) 2019-2024 LiLong                                              **
+**  This file is part of QCustomUi.                                             **
+**                                                                              **
+**  QCustomUi is free software: you can redistribute it and/or modify           **
+**  it under the terms of the GNU Lesser General Public License as published by **
+**  the Free Software Foundation, either version 3 of the License, or           **
+**  (at your option) any later version.                                         **
+**                                                                              **
+**  QCustomUi is distributed in the hope that it will be useful,                **
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of              **
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               **
+**  GNU Lesser General Public License for more details.                         **
+**                                                                              **
+**  You should have received a copy of the GNU Lesser General Public License    **
+**  along with QCustomUi.  If not, see <https://www.gnu.org/licenses/>.         **
+**********************************************************************************/
+
+#pragma once
+
+#include "qcustomui_global.h"
+
+#include <QGraphicsObject>
+#include <QString>
+
+#include <memory>
+
+class QCUSTOMUI_EXPORT QCtmFlowChartNode : public QGraphicsObject
+{
+public:
+    enum class Type
+    {
+        Start,
+        Flow,
+        Judgment,
+        Manual,
+        Line,
+        End,
+    };
+    QCtmFlowChartNode();
+    virtual ~QCtmFlowChartNode();
+    virtual Type type() const                   = 0;
+    virtual bool allowLinkType(Type type) const = 0;
+    virtual const std::vector<QCtmFlowChartNode>& nexts() const;
+    virtual void setText(const QString& text);
+    virtual QString text() const;
+signals:
+    void textChanged(const QString& text);
+
+private:
+    struct Impl;
+    std::unique_ptr<Impl> m_impl;
+};
+
+class QCUSTOMUI_EXPORT QCtmFlowChartStartNode : public QCtmFlowChartNode
+{
+public:
+};
